e771bdc9bca8172eb0a5f3876ce2b30b020cc0ed
Anton Akulov <antonakv@users.noreply.github.com>
2022-09-07 11:01:01 +0200

    adding net-data package

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,22 +1,31 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 export APTARGS="-qq -o=Dpkg::Use-Pty=0"
 export DEBIAN_FRONTEND=noninteractive
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get clean ${APTARGS}
-sudo DEBIAN_FRONTEND=noninteractive apt-get update ${APTARGS}
+sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes clean ${APTARGS}
+sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes update ${APTARGS}
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y ${APTARGS}
+sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes upgrade ${APTARGS}
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y ${APTARGS} cloud-utils ctop htop git vim curl wget tar software-properties-common htop unattended-upgrades gpg-agent apt-transport-https ca-certificates thin-provisioning-tools net-tools 
+sudo DEBIAN_FRONTEND=noninteractive apt-get install --assume-yes ${APTARGS} net-tools htop git vim curl wget tar software-properties-common htop unattended-upgrades gpg-agent apt-transport-https ca-certificates thin-provisioning-tools
 
 sudo unattended-upgrades -v
 
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-sudo echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y update ${APTARGS}
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install docker-ce=5:20.10.7~3-0~ubuntu-focal docker-ce-cli=5:20.10.7~3-0~ubuntu-focal containerd.io awscli jq neovim unzip ${APTARGS}
+
+curl -s https://packagecloud.io/install/repositories/netdata/netdata/script.deb.sh | sudo DEBIAN_FRONTEND=noninteractive bash
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes update ${APTARGS}
+sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes install netdata ${APTARGS}
+sudo sed -i -e 's/localhost/0.0.0.0/g' /etc/netdata/netdata.conf
+sudo sed -i -e 's/\[global\]/\[global\]\n    update every = 2/' /etc/netdata/netdata.conf
+
+sudo systemctl enable netdata.service
---
+++ template.pkr.hcl
@@ -0,0 +1,35 @@
+packer {
+  required_plugins {
+    amazon = {
+      version = "~> 1.1.2"
+      source  = "github.com/hashicorp/amazon"
+    }
+  }
+}
+
+source "amazon-ebs" "ubuntu" {
+  ami_name      = "packer-aws-ubuntufocal-13"
+  instance_type = "t3.large"
+  region        = "eu-central-1"
+  source_ami_filter {
+    filters = {
+      name                = "ubuntu/images/*ubuntu-focal-*-amd64-server-**"
+      root-device-type    = "ebs"
+      virtualization-type = "hvm"
+    }
+    most_recent = true
+    owners      = ["099720109477"]
+  }
+  ssh_username = "ubuntu"
+}
+
+build {
+  name    = "ubuntufocal-tfe"
+  sources = [
+    "source.amazon-ebs.ubuntu"
+  ]
+
+  provisioner "shell" {
+    script = "scripts/packages.sh"
+  }
+}
